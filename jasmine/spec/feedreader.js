910b5d0202aa6f65014df5fab12cb7fbf8085dfa
Saket Pareek <saket.pareek90@gmail.com>
2018-09-13 16:15:21 +0530

    ckeck if feed has a single entry

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -88,6 +88,10 @@ $(function() {
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
+    describe('Initial Entries', function(){
+
+
+
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
@@ -95,10 +99,31 @@ $(function() {
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
 
+         beforeEach(function(done){
+           loadFeed(0, function(){
+             done();
+           });
+         });
+
+         it('check if it has atleast one entry', function(){
+           expect($('.feed')).toBeDefined();
+           expect($('.entry')).toBeDefined();
+         });
+
+    });
+
     /* TODO: Write a new test suite named "New Feed Selection" */
+
+    describe('New Feed Selection', function(){
+
+
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+
+    });     
+
+
 }());
